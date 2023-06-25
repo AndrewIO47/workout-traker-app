50186a353321223ff23a02b81aeb3aa21798927f
Andrew Chiang <andrew.chiang47@gmail.com>
2023-06-24 20:01:00 -0500

    added endpoints for each page

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static', static_folder='static')
 
 @app.route('/')
 def index():
-    return "Hello World"
+    return render_template('index.html')
+
+@app.route('/main-page')
+def main_page():
+    return render_template('main-page.html')
+
+@app.route('/start-workout')
+def start_workout():
+    return render_template('start-workout.html')
+
+@app.route('/workout-log')
+def workout_log():
+    return render_template('workout-log.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
